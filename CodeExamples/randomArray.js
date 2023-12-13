d38fec0f0f6857b5978522549383c5e1351f4d67
John Blalock <simpledimplejohn@gmail.com>
2023-12-13 14:12:56 -0800

    randomarry needs the array updated

--- CodeExamples/randomArray.js
+++ CodeExamples/randomArray.js
@@ -12,23 +12,17 @@ const randnum = () => {
         return rand
 }
 
-
-
-
 // challege, take an array of names, create an array of objects that contains the name and an age
 
 const nameArr = ["bob", "fred", "tom", "buzz", "max"];
-const color = ["blue","red","orange","red","green"];
 const ageArr = [];
 let person = {name: "",age: 0};
 
-for(names in nameArr) {
-    console.log(name)
+
+for (let i = 0; i < nameArr.length; i ++) {
+    person.name = nameArr[i]
+    person.age = randnum()
+    ageArr.push(person)
 }
 
-// for(names in name) {
-//     person.name = name[names];
-//     person.age = randnum()
-//     ageArr.push(person) 
-// }
 console.log(ageArr)
