833be4b6abd32f3b7a708def250d9de561e7a0f1
John Blalock <simpledimplejohn@gmail.com>
2024-01-02 13:20:31 -0800

    update

---
+++ notes/data_structures.js
@@ -0,0 +1,28 @@
+/* 
+Data Structure Notes For Javascript
+
+Objects 
+
+Map
+
+*/
+
+
+// OBJECTS
+let newObject = {}
+
+let a = 1
+let b = 2
+
+newObject[a] = b
+
+// console.log(newObject)
+
+
+// MAP
+const newMap = new Map();
+
+newMap.set(a,b)
+newMap.set(7,111)
+newMap.set("po-ta-to","po-taugh-to")
+console.log(newMap)
