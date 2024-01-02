2d6df2eb26cb01e3d6c391e27de4386f95966bde
John Blalock <simpledimplejohn@gmail.com>
2024-01-02 13:59:28 -0800

    udpate

--- notes/data_structures.js
+++ notes/data_structures.js
@@ -6,23 +6,36 @@ Objects
 Map
 
 */
-
+// Array
+let theArray = [3,6,7, 3,9,11,2,9]
+theArray.push(5)
+// console.log(theArray)
 
 // OBJECTS
 let newObject = {}
-
 let a = 1
 let b = 2
-
 newObject[a] = b
 
 // console.log(newObject)
 
 
 // MAP
+// stores key values pairs, key must be a string?  Fast search
 const newMap = new Map();
 
 newMap.set(a,b)
 newMap.set(7,111)
 newMap.set("po-ta-to","po-taugh-to")
-console.log(newMap)
+// console.log(newMap)
+
+
+// SET 
+// distinct data types, fast search
+const newSet = new Set();
+
+for (let num in theArray) {
+    newSet.add(theArray[num])
+}
+console.log(newSet)
+console.log("has returns boolean of: ",newSet.has(3))
